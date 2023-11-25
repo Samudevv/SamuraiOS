bb0bce0fa82884fd7fe77d653eb238cd37f2b995
PucklaJ <jonaas.pucher000000@gmail.com>
2023-11-25 18:59:15 +0100

    Make nvim init more fancy and add W and Q

--- home/ninja/.config/nvim/init.lua
+++ home/ninja/.config/nvim/init.lua
@@ -14,10 +14,24 @@ local function bootstrap_pckr()
 	vim.opt.rtp:prepend(pckr_path)
 end
 
-local bind = vim.keymap.set
+local function cmd(args)
+	local value = 'command! -nargs=0 ' .. args
+	vim.cmd(value)
+end
 
 bootstrap_pckr()
 
+local bind = vim.keymap.set
+local c = vim.cmd
+local slt = { silent = true }
+
+--  ____  _             _           
+-- |  _ \| |_   _  __ _(_)_ __  ___ 
+-- | |_) | | | | |/ _` | | '_ \/ __|
+-- |  __/| | |_| | (_| | | | | \__ \
+-- |_|   |_|\__,_|\__, |_|_| |_|___/
+--                |___/             
+--
 require('pckr').add{
 	'Mofiqul/dracula.nvim',
 	'mg979/vim-visual-multi',
@@ -28,15 +42,46 @@ require('pckr').add{
 local fzf = require('fzf-lua')
 local bufferline = require('bufferline')
 
-vim.cmd('set number')
-vim.cmd('colorscheme dracula')
 fzf.setup({'fzf-native'})
 vim.opt.termguicolors = true
 bufferline.setup{}
-
-bind('n', '<c-P>', '<cmd>lua require("fzf-lua").git_files()<CR>', { silent = true })
-bind('n', '<Tab>', '<cmd>bnext<CR>',                              { silent = true })
-bind('n', '<c-W>', '<cmd>bd<CR>',                                 { silent = true })
-bind('n', '<c-Q>', '<cmd>q<CR>',                                  { silent = true })
-bind('n', '<c-S>', '<cmd>w<CR>',                                  { silent = true })
+--  _  __          _                         _ 
+-- | |/ /___ _   _| |__   ___   __ _ _ __ __| |
+-- | ' // _ \ | | | '_ \ / _ \ / _` | '__/ _` |
+-- | . \  __/ |_| | |_) | (_) | (_| | | | (_| |
+-- |_|\_\___|\__, |_.__/ \___/ \__,_|_|  \__,_|
+--           |___/                             
+--  ____  _           _ _                 
+-- | __ )(_)_ __   __| (_)_ __   __ _ ___ 
+-- |  _ \| | '_ \ / _` | | '_ \ / _` / __|
+-- | |_) | | | | | (_| | | | | | (_| \__ \
+-- |____/|_|_| |_|\__,_|_|_| |_|\__, |___/
+--                              |___/     
+bind('n', '<c-P>', '<cmd>lua require("fzf-lua").git_files()<CR>', slt)
+bind('n', '<Tab>', '<cmd>bnext<CR>',                              slt)
+bind('n', '<c-W>', '<cmd>bd<CR>',                                 slt)
+bind('n', '<c-Q>', '<cmd>q<CR>',                                  slt)
+bind('n', '<c-S>', '<cmd>w<CR>',                                  slt)
+--   ____          _                  
+--  / ___|   _ ___| |_ ___  _ __ ___  
+-- | |  | | | / __| __/ _ \| '_ ` _ \ 
+-- | |__| |_| \__ \ || (_) | | | | | |
+--  \____\__,_|___/\__\___/|_| |_| |_|
+--                                    
+--   ____                                          _     
+--  / ___|___  _ __ ___  _ __ ___   __ _ _ __   __| |___ 
+-- | |   / _ \| '_ ` _ \| '_ ` _ \ / _` | '_ \ / _` / __|
+-- | |__| (_) | | | | | | | | | | | (_| | | | | (_| \__ \
+--  \____\___/|_| |_| |_|_| |_| |_|\__,_|_| |_|\__,_|___/
+--
+cmd('W write')
+cmd('Q quit')
+--   ____                                          _     
+--  / ___|___  _ __ ___  _ __ ___   __ _ _ __   __| |___ 
+-- | |   / _ \| '_ ` _ \| '_ ` _ \ / _` | '_ \ / _` / __|
+-- | |__| (_) | | | | | | | | | | | (_| | | | | (_| \__ \
+--  \____\___/|_| |_| |_|_| |_| |_|\__,_|_| |_|\__,_|___/
+--
+c('set number')
+c('colorscheme dracula')
 
