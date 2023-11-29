916fac959ca8671a9d65b9ccf691119757f8ad59
PucklaJ <jonaas.pucher000000@gmail.com>
2023-11-29 21:32:37 +0100

    Create directory of extlinux.conf

--- install.go
+++ install.go
@@ -1152,6 +1152,8 @@ func writeExtlinuxConf(mountPath, filePath string) {
 	uuidEnd := strings.IndexRune(uuidAndSuffix, '"')
 	uuid := uuidAndSuffix[:uuidEnd]
 
+	dir := filepath.Dir(filePath)
+	exe("mkdir -p " + dir)
 	file, err := os.Create(filePath)
 	if err != nil {
 		logError(err)
