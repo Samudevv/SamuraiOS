d369a856b390f041f3600c400a421fcc96a7e7d0
PucklaJ <jonaas.pucher000000@gmail.com>
2024-01-23 18:36:07 +0100

    Fix compile_style.sh

--- home/ninja/.config/compile_style.sh
+++ home/ninja/.config/compile_style.sh
@@ -10,12 +10,12 @@ RESET='\033[0m'
 ln -sfr $script_dir/light_colors.scss $script_dir/colors.scss
 
 echo -e $BLUE Compiling all light stylesheets ... $RESET
-GREEN=$GREEN BLUE=$BLUE RESET=$RESET $script_dir/waybar/compile_light_style.sh
+GREEN=$GREEN BLUE=$BLUE RESET=$RESET $script_dir/waybar/compile_style.sh light
 
 ln -sfr $script_dir/dark_colors.scss $script_dir/colors.scss
 
 echo -e $BLUE Compiling all dark stylesheets ... $RESET
-GREEN=$GREEN BLUE=$BLUE RESET=$RESET $script_dir/waybar/compile_dark_style.sh
+GREEN=$GREEN BLUE=$BLUE RESET=$RESET $script_dir/waybar/compile_style.sh dark
 
 rm $script_dir/colors.scss
 
